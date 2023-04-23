9d1154cc3e1a0d1d72088f7399508f3f6375cf4d
Caio <alcarriacaio@gmail.com>
2023-04-23 15:12:40 -0300

    v1

--- src/utils/userData.ts
+++ src/utils/userData.ts
@@ -1,9 +1,9 @@
 export const userData = {
-  nameUser: "Samuel Leão",
-  githubUser: "samuelleaokenzie",
-  linkedinUser: "nekelpatrick",
-  whatsappNumber: "+5541988819028",
-  emailUser: "samuel@kenzie.com.br",
-  instagramUser: "samuelleaoui",
+  nameUser: "Caio Alcarria",
+  githubUser: "caioalcarria",
+  linkedinUser: "caioalcarria",
+  whatsappNumber: "+5571999332003",
+  emailUser: "alcarriacaio@gmail.com",
+  instagramUser: "caioalcarria",
   facebookUser: "",
 };
